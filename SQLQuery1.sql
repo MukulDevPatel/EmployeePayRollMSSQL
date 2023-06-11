375edcbcfc5b68018d482de9d5d594a29c0b5ca8
MukulDevPatel <patelmukuldev@gmail.com>
2023-06-11 18:08:48 +0530

    [Mukul Dev] Add: We are retrieve salary for a particular employee and show employees who have joined in a particular data range

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -12,14 +12,18 @@ StartDate Date
 );
 
 --UC3_InertData
-Insert into Employee_Payroll(Name,Salary,StartDate)values('Mukul',20000,'2022-10-10');
-Insert into Employee_Payroll(Name,Salary,StartDate)values('Steve',30000,'2022-10-12');
-Insert into Employee_Payroll(Name,Salary,StartDate)values('Vansh',20000,'2022-10-8');
-Insert into Employee_Payroll(Name,Salary,StartDate)values('Dev',21000,'2022-10-8');
-Insert into Employee_Payroll(Name,Salary,StartDate)values('Bruce',22000,'2022-10-8');
-Insert into Employee_Payroll(Name,Salary,StartDate)values('Arvind',31000,'2022-10-8');
-Insert into Employee_Payroll(Name,Salary,StartDate)values('Chris',32000,'2022-10-8');
-Insert into Employee_Payroll(Name,Salary,StartDate)values('Jay',40000,'2022-10-8');
+Insert into Employee_Payroll(Name,Salary,StartDate)values('Mukul',20000,'2020-10-10');
+Insert into Employee_Payroll(Name,Salary,StartDate)values('Steve',30000,'2020-10-12');
+Insert into Employee_Payroll(Name,Salary,StartDate)values('Vansh',20000,'2020-10-8');
+Insert into Employee_Payroll(Name,Salary,StartDate)values('Dev',21000,'2020-10-8');
+Insert into Employee_Payroll(Name,Salary,StartDate)values('Bruce',22000,'2020-10-8');
+Insert into Employee_Payroll(Name,Salary,StartDate)values('Arvind',31000,'2020-10-8');
+Insert into Employee_Payroll(Name,Salary,StartDate)values('Chris',32000,'2020-10-8');
+Insert into Employee_Payroll(Name,Salary,StartDate)values('Jay',40000,'2020-10-8');
 
 --UC4_RetrieveAllData
 select * from Employee_Payroll;
+
+--UC5_RetrieveSalary
+select salary from Employee_Payroll WHERE Name = 'Chris';
+select * from Employee_Payroll where StartDate between CAST('2022-06-03'as Date) and CURRENT_TIMESTAMP;

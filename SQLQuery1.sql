27da519ace9e19997b712471fa7640ba33426730
MukulDevPatel <patelmukuldev@gmail.com>
2023-06-11 17:32:03 +0530

    [Mukul Dev] Add: We are inserting some data in employee payroll database table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -10,3 +10,13 @@ Name varchar(20),
 Salary Bigint,
 StartDate Date
 );
+
+--UC3_InertData
+Insert into Employee_Payroll(Name,Salary,StartDate)values('Mukul',20000,'2022-10-10');
+Insert into Employee_Payroll(Name,Salary,StartDate)values('Steve',30000,'2022-10-12');
+Insert into Employee_Payroll(Name,Salary,StartDate)values('Vansh',20000,'2022-10-8');
+Insert into Employee_Payroll(Name,Salary,StartDate)values('Dev',21000,'2022-10-8');
+Insert into Employee_Payroll(Name,Salary,StartDate)values('Bruce',22000,'2022-10-8');
+Insert into Employee_Payroll(Name,Salary,StartDate)values('Arvind',31000,'2022-10-8');
+Insert into Employee_Payroll(Name,Salary,StartDate)values('Chris',32000,'2022-10-8');
+Insert into Employee_Payroll(Name,Salary,StartDate)values('Jay',40000,'2022-10-8');

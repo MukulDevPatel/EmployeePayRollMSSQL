5ba7ff9888e56bbff612329c6fdc4d9c1cffc781
MukulDevPatel <patelmukuldev@gmail.com>
2023-06-11 17:23:51 +0530

    [Mukul Dev] Add: We create employee payroll table for inserting data

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -2,3 +2,11 @@ create database Payroll_Service;
 
 --UC1_CreateDatabse
 use Payroll_Service;
+
+--UC2_EmployeePayroll
+create table Employee_Payroll(
+ID int identity(1,1) Primary Key,
+Name varchar(20),
+Salary Bigint,
+StartDate Date
+);
